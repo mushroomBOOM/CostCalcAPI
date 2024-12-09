7dd44c699901e9f84efe776a3183feed5e247fbe
karko <karko@DESKTOP-VA6G6PC>
2024-12-09 14:49:09 +0300

    Добавьте файлы проекта.

---
+++ CostCalcAPI/Migrations/20241120121330_AddProjectIdToOverheadCost.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CostCalcAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectIdToOverheadCost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OverheadCosts_Projects_ProjectId",
+                table: "OverheadCosts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectId",
+                table: "OverheadCosts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OverheadCosts_Projects_ProjectId",
+                table: "OverheadCosts",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OverheadCosts_Projects_ProjectId",
+                table: "OverheadCosts");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectId",
+                table: "OverheadCosts",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OverheadCosts_Projects_ProjectId",
+                table: "OverheadCosts",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id");
+        }
+    }
+}
